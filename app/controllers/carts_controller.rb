4946b7d1f0d2165b5f7b2e8cdca9125983f9e23c
osha7 <ogroetz@gmail.com>
2020-08-01 17:31:31 -0600

    correction of controller instances for serialized

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,12 @@
 class CartsController < ApplicationController
 
     def index
-        @carts = Cart.all 
+        carts = Cart.all 
         render json: CartSerializer.new(carts).to_serialized_json
     end
 
     def show
-        @cart = Cart.find_by(id: params)
+        cart = Cart.find_by(id: params)
         render json: CartSerializer.new(cart).to_serialized_json
     end
 
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,12 @@
 class ItemsController < ApplicationController
 
     def index
-        @items = Item.all 
+        items = Item.all 
         render json: ItemSerializer.new(items).to_serialized_json
     end
 
     def show
-        @item = Item.find_by(id: params)
+        item = Item.find_by(id: params)
         render json: ItemSerializer.new(item).to_serialized_json
     end
 
