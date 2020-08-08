dcc97549aa544462532fa02498fcb4b4869b0d72
osha7 <ogroetz@gmail.com>
2020-08-08 15:26:11 -0600

    fix on cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,5 +2,5 @@ class Cart < ApplicationRecord
     has_many :items
 
     # validates :item, uniqueness: { scope: :cart}
-    validates :item, uniqueness: true
+    # validates :item, uniqueness: true
 end
