af26098ad40437ad52f10642791909a0e2860b85
osha7 <ogroetz@gmail.com>
2020-11-06 12:10:18 -0700

    comment on root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # patch '/items/:id', to: "items#update"
 
   get '/create-or-return-cart', to: "carts#create_or_return_cart"
+
+  # equivalent to:
+  # get "/", "application#home" 
   root to: 'application#home'
 
 end
